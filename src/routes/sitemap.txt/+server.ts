9072c7c133b03e95a6ac7770bb92f9ff94d5f400
Aleksandr Nefedov <garret.rus@gmail.com>
2025-11-20 22:37:16 +0300

    Sitemap

---
+++ src/routes/sitemap.txt/+server.ts
@@ -0,0 +1,21 @@
+import { load } from '../+page.server';
+
+export async function GET() {
+    const origin = 'https://4eb0da.ru';
+    const urls = [
+        `${origin}/`,
+        `${origin}/about`,
+    ];
+
+    const posts = (await load()).posts;
+
+    for (const post of posts) {
+        urls.push(`${origin}/posts/${post.name}`);
+    }
+
+    return new Response(urls.join('\n'), {
+        headers: {
+            'Content-Type': 'text/plain'
+        }
+    });
+}

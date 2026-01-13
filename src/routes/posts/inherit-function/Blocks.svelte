cf7b7363f5531d8eafa5d98b616651e04c952b4b
Aleksandr Nefedov <garret.rus@gmail.com>
2026-01-13 21:58:23 +0300

    Inherit function

---
+++ src/routes/posts/inherit-function/Blocks.svelte
@@ -0,0 +1,22 @@
+<div class="block" style:--level={1}>
+    Block level 1
+
+    <div class="block" style:--level={2}>
+        Block level 2
+
+        <div class="block" style:--level={3}>Block level 3</div>
+        <div class="block" style:--level={3}>Block level 3</div>
+    </div>
+</div>
+
+<style>
+    .block {
+        --accent: 220;
+        --saturation: 50;
+
+        margin: 10px;
+        padding: 10px;
+        color: #000;
+        background-color: hsl(var(--accent) var(--saturation) calc(100 - 10 * var(--level)));
+    }
+</style>
---
+++ src/routes/posts/inherit-function/Tree.svelte
@@ -0,0 +1,22 @@
+<div class="tree-item" style:--level={1}>
+    <div class="tree-item__name">--level: 1</div>
+    <div class="tree-item" style:--level={2}>
+        <div class="tree-item__name">--level: 2</div>
+        <div class="tree-item" style:--level={3}>
+            <div class="tree-item__name">--level: 3</div>
+        </div>
+    </div>
+</div>
+
+<style>
+    .tree-item__name {
+        display: flex;
+        align-items: center;
+        height: 32px;
+        padding-left: calc(var(--level) * 20px);
+    
+        &:hover {
+            background-color: rgb(45, 157, 194);
+        }
+    }
+</style>

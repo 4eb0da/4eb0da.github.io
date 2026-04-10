eadc8157be624adddefaad643d4db5c1aec2fcc1
Aleksandr Nefedov <garret.rus@gmail.com>
2026-04-10 23:35:26 +0300

    WebTransport

---
+++ src/routes/posts/webtransport-is-a-king/Table.svelte
@@ -0,0 +1,80 @@
+<div class="scroll">
+    <table>
+        <thead>
+            <tr>
+                <th>
+                    АПИ
+                </th>
+                <th>
+                    Назначение
+                </th>
+                <th>
+                    Порядок данных
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>
+                    Поллинг
+                </td>
+                <td>
+                    Браузер-Сервер
+                </td>
+                <td>
+                    Гарантированный
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    WebSocket
+                </td>
+                <td>
+                    Браузер-Сервер
+                </td>
+                <td>
+                    Гарантированный
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    WebRTC Data Channel
+                </td>
+                <td>
+                    Браузер-Браузер, Браузер-Сервер*
+                </td>
+                <td>
+                    Гарантированный + Негарантированный
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    WebTransport
+                </td>
+                <td>
+                    Браузер-Сервер
+                </td>
+                <td>
+                    Гарантированный + Негарантированный
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+
+<style>
+    .scroll {
+        width: fit-content;
+        max-width: 100%;
+        overflow-x: auto;
+    }
+
+    table {
+        border-collapse: collapse;
+    }
+
+    th, td {
+        padding: 4px 8px;
+        border: 1px solid;
+    }
+</style>
